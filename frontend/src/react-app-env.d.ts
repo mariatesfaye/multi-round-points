b218305b0c5df9f6900f94a4ca51830da338e797
Maria Tesfaye <mariatesfaye67@gmail.com>
2025-08-12 14:20:21 +0300

    Delete frontend directory

--- frontend/src/react-app-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react-scripts" />
-
-interface ImportMetaEnv {
-  VITE_API_URL: string;
-  VITE_REVERB_APP_KEY: string;
-  VITE_REVERB_HOST: string;
-  VITE_REVERB_PORT: string;
-  VITE_REVERB_CLUSTER?: string;
-}
